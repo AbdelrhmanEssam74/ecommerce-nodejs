6635dfcfdad7ee84122fed4782cc6a60d4e0735d
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-15 16:02:26 +0300

    Add initial project structure with authentication and DB setup
    Set up the foundational structure for the project, including controllers, routes, middleware, and a database connection. Implemented initial authController logic and integrated MySQL database configuration. Added basic assets and views for the application.

---
+++ DB/db-connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql2')
+console.time('db-connect');
+const connection = mysql.createConnection({
+    host: 'sql8.freesqldatabase.com',
+    user: 'sql8778978',
+    password: 'adeVXEaYDe',
+    database: 'sql8778978',
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
+})
+
+connection.connect((err) => {
+    console.timeEnd('db-connect');
+    if (err) {
+        console.log('Error connecting to the database:', err)
+        return
+    }
+    console.log('Connected to the database')
+})
+
+module.exports = connection;
