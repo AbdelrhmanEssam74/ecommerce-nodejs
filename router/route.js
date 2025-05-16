97f88dbc1b327c996cea9f3fe92dbe82bfd43f65
Abdelrhman <abdelrhmanroshdy8@gmail.com>
2025-05-17 02:57:15 +0300

    Add PayPal order creation route
    Introduced a new POST route '/paypal/create-order' to the router, handled by the PayPal controller. This enables creating PayPal orders via the API.

--- router/route.js
+++ router/route.js
@@ -4,6 +4,8 @@ const router = express.Router();
 const AddressController = require('../Controllers/addressController');
 const ShippingController = require('../Controllers/shippingController')
 const CheckoutController = require('../Controllers/checkoutController')
+const paypalController = require('../Controllers/paypalController');
+
 
 router.post('/address', AddressController.address);
 router.get('/address/:userId', AddressController.getAddress);
@@ -11,4 +13,6 @@ router.get('/address/:userId', AddressController.getAddress);
 router.get('/shipping', ShippingController.getShippingOptions)
 
 router.post('/checkout' , CheckoutController.createOrder)
+
+router.post('/paypal/create-order', paypalController.createPayPalOrder);
 module.exports = router;
